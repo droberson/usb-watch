b12aa1d447942b6c6b864347d96f2ebf5fb5f87a
Daniel Roberson <daniel@planethacker.net>
2017-06-28 10:12:07 -0700

    initial commit

---
+++ example_settings.py
@@ -0,0 +1,18 @@
+### WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
+###
+### This file MUST be edited properly and copied to settings.py in order for
+### SMS functionality to work. Get set up on Twilio.com for the required API
+### keys and phone number settings.
+###
+### WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
+
+# These are Twilio API settings. Sign up at Twilio to get them.
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# The number the SMS is from. Must be valid on your Twilio account.
+phone_from="+1213XXXYYYY"
+
+# The number to send the SMS to (your cell phone number)
+phone_to="+1808XXXYYYY"
+
